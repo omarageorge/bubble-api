6bc1033752495c80b26d7f8ef30d8ba8dca10dc6
omarageorge <georgeomara8@gmail.com>
2022-02-11 14:37:52 +0300

    Added Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { ControllerModule } from './auth/controller/controller.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/bubble')],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/bubble'), ControllerModule, AuthModule],
+  providers: [AuthService],
 })
 export class AppModule {}
