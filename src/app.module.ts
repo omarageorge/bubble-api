b67a66308241c666923692726df15e20efcaff06
omarageorge <georgeomara8@gmail.com>
2022-02-11 16:02:04 +0300

    Created Auth folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
-import { ControllerModule } from './auth/controller/controller.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/bubble'), ControllerModule, AuthModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost/bubble'),
+  ],
 })
 export class AppModule {}
