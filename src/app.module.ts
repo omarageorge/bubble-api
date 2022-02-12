c15716edcb690f0a1407475aaa71d171d374a470
omarageorge <georgeomara8@gmail.com>
2022-02-12 17:24:01 +0300

    Added Transaction setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/bubble'),
     UsersModule,
     AuthModule,
+    TransactionsModule,
   ],
   controllers: [AppController],
 })
